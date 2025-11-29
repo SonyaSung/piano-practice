22bdb10893a4f49876d2ef4b8132afb7d7dd9a83
Sonyasung <sungsonya@gmail.com>
2025-11-29 08:59:02 +0800

    Remove importmap from index.html

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,11 @@
-export {};
+// Removed invalid reference to vite/client which caused "Cannot find type definition file" error
+// /// <reference types="vite/client" />
 
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      API_KEY: string;
-      [key: string]: string | undefined;
-    }
+// Fix for "Cannot redeclare block-scoped variable 'process'"
+// Augment the existing NodeJS.ProcessEnv interface to include API_KEY instead of redeclaring process
+declare namespace NodeJS {
+  interface ProcessEnv {
+    API_KEY: string;
+    [key: string]: string | undefined;
   }
 }
